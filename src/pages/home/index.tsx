f0e39d9b77c224cd503b28803b90b2a0cc770398
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-22 06:24:19 -0800

    CEOProfile Has been completed and designed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "../../components/banner";
+import CEOProfile from "../../components/home";
 
-const sliderImages = [
+const sliderImages: string[] = [
   "/images/home/containerpic1.jpg",
   "/images/home/containerpic2.jpg",
   "/images/home/containerpic3.jpg",
@@ -8,12 +9,16 @@ const sliderImages = [
 
 function HomePage() {
   return (
-    <HeroSection
-      backgroundimage="/images/home/containerpic1.jpg"
-      title="Ad Aim Marketing Agency"
-      description="Where precision meets impact, delivering excellence for your brand’s success"
-      sliderImages={sliderImages}
-    />
+    <div>
+      <HeroSection
+        backgroundimage="/images/home/containerpic1.jpg"
+        title="Ad Aim Marketing Agency"
+        description="Where precision meets impact, delivering excellence for your brand’s success"
+        sliderImages={sliderImages}
+      />
+
+      <CEOProfile />
+    </div>
   );
 }
 
