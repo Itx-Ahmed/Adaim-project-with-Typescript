73ebe0218f4bb9c44336ff1e8b12a055a531bea6
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-01-23 23:54:36 -0800

    social-Media-cards designed

--- src/components/SocialMediaCards/index.tsx
+++ src/components/SocialMediaCards/index.tsx
@@ -2,39 +2,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 type TSocialMediaServiceCardProps = {
   title: string;
-  services: {
-    title: string;
-    description: string;
-    icon: any;
-  }[];
+  description: string;
+  icon: any;
 };
 
 function SocialMediaCard(props: TSocialMediaServiceCardProps) {
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-3xl font-bold text-center mb-8 mt-[7rem]">
-        {props.title}
-      </h1>
-      <div className="flex flex-wrap justify-center gap-6">
-        {props.services.map((service, index) => (
-          <div
-            key={index}
-            className="bg-white shadow-lg rounded-lg p-6 flex flex-col justify-between w-full sm:w-1/2 md:w-1/3 lg:w-1/4 transform transition-transform duration-300 ease-in-out hover:scale-105"
-          >
-            <div className="flex flex-col items-center mb-4">
-              <h2 className="text-2xl font-semibold text-center mb-4">
-                {service.title}
-              </h2>
-              <p className="text-gray-600 mb-4">{service.description}</p>
-              <div className="flex justify-center mt-4">
-                <FontAwesomeIcon
-                  icon={service.icon}
-                  className="text-2xl sm:text-xl md:text-2xl lg:text-3xl text-blue-500" // Responsive size
-                />
-              </div>
-            </div>
-          </div>
-        ))}
+    <div className="max-w-xs mx-auto bg-white border border-gray-300 shadow-lg rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-95 mt-3">
+      <div className="p-3 text-center">
+        <div className="text-4xl text-blue-600 mb-4">
+          <FontAwesomeIcon icon={props.icon} />
+        </div>
+        <h2 className="text-xl font-semibold text-gray-800">{props.title}</h2>
+        <p className="mt-1 text-gray-600">{props.description}</p>
       </div>
     </div>
   );
