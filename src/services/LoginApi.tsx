e886b6eefc1ee2eb6db738044349cb1b0f983714
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-02-02 04:37:13 -0800

    remove pokemon-api

--- src/services/LoginApi.tsx
+++ src/services/LoginApi.tsx
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const loginApi = createApi({
   reducerPath: "loginApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3009/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3009/api/login" }),
   endpoints: (builder) => ({
     loginUser: builder.mutation({
       query: (userData) => ({
-        url: "auth/login",
+        url: "/api/login",
         method: "POST",
         body: userData,
       }),
