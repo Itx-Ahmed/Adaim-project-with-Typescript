d7af44d2d25ccc877b7e1e2dacfacf546baa9c0a
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-02-06 00:13:28 -0800

    Resolved merge conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import { Provider } from "react-redux";
 import { store } from "./store";
-=======
->>>>>>> main
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-<<<<<<< HEAD
     <Provider store={store}>
       <App />
     </Provider>
-=======
     <App />
->>>>>>> main
   </StrictMode>
 );
