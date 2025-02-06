02e028f37533b58bb9a13296d90b6911428f1e66
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-02-06 00:21:49 -0800

    remove conflicts from store

--- src/store.tsx
+++ src/store.tsx
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-<<<<<<< HEAD
 import { loginApi } from "./services/LoginApi";
 
 export const store = configureStore({
@@ -10,17 +9,6 @@ export const store = configureStore({
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(loginApi.middleware),
-=======
-import { UsersApi } from "./services/Users";
-
-export const store = configureStore({
-  reducer: {
-    [UsersApi.reducerPath]: UsersApi.reducer,
-  },
-
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(UsersApi.middleware),
->>>>>>> main
 });
 
 setupListeners(store.dispatch);
